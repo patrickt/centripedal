c023cf3bcf71c4217e9813e2559ace39d94701f2
Patrick Thomson <patrickt@github.com>
2021-11-12 14:39:01 -0500

    Initial Commit

---
+++ centripedal/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  centripedal
+//
+//  Created by Patrick Thomson on 11/12/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
