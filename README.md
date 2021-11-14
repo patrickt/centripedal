bcc1bc08eed2abc6b1b889f66abe64adabbb6070
Patrick Thomson <patrickt@github.com>
2021-11-14 16:47:35 -0500

    it's actually nice now

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-This is a tiny little daemon (meant to be run as root) that (hopefully) coalesces pedal presses on my Kinesis foot pedals and my external Keymacs keyboard.
+# centripedal
 
-Use at your own risk. If something gets stuck you may have to restart your computer.
+This is a tiny little daemon for macOS that coalesces keyboard modifier events (Ctrl, Alt, Cmd) between different keyboards (or other human interface devices like foot pedals) that are plugged into the same machine. By default, macOS keeps keyboard states independently of each other, so that pressing F on keyboard A while holding down CTRL on keyboard B is recognized only as an F. Though this behavior is sensible for an OS default, it interferes with workflows that use a dedicated input device for modifier events.
+
+You'll need to grant this app accessibility privileges (in System Preferences.app) or run it as root.
+
+## How it works
+
+This uses the Quartz `CGEventTap` API to install a listener that listens for modifier-key events and adds them to other key-down events if necessary. Its state resets 10 seconds after the last modifier key is pressed, just to ensure that the system never gets in an unusable state. (In the event that it does, logging out and in will cure it.)
+
+## To build
+
+Run `xcodebuild`.
 
 GPL3
--- centripedal/main.c
+++ centripedal/main.c
@@ -1,4 +1,4 @@
-// -*- mode: c; c-basic-offset: 4; compilation-scroll-output: t; compile-command: "xcodebuild"-*-
+// -*- mode: c; c-basic-offset: 4; compile-command: "xcodebuild"-*-
 //
 //  main.c
 //  centripedal
@@ -11,55 +11,9 @@
 #include <CoreGraphics/CoreGraphics.h>
 #include <getopt.h>
 
-static int debug, dry_run;
+static int debug, dry_run, help;
 
-// TODO: we could just use CGEventFlags here.
-// also TODO: there should be a CFRunLoopTimer here
-struct kb_state {
-    _Bool pedal_ctrl_down : 1;
-    _Bool pedal_alt_down : 1;
-    _Bool pedal_cmd_down : 1;
-} __attribute__((packed));
-
-#define TOGGLE(state, it) if (it && !state) state = true
-
-CGEventRef pedalCallback(CGEventTapProxy proxy __attribute__((unused)), CGEventType type, CGEventRef event, void *info) {
-    struct kb_state *state = (struct kb_state *)info;
-    CGEventFlags flags = CGEventGetFlags(event);
-    if (type == kCGEventFlagsChanged) {
-        if (debug) printf("flags: %d %d %d\n", state->pedal_ctrl_down, state->pedal_alt_down, state->pedal_cmd_down);
-        TOGGLE(state->pedal_ctrl_down, flags & kCGEventFlagMaskControl);
-        TOGGLE(state->pedal_alt_down, flags & kCGEventFlagMaskAlternate);
-        TOGGLE(state->pedal_cmd_down, flags & kCGEventFlagMaskCommand);
-        if (debug) printf("after: %d, %d, %d\n", state->pedal_ctrl_down, state->pedal_alt_down, state->pedal_cmd_down);
-    } else if (type == kCGEventKeyDown) {
-        CFTimeInterval since = CGEventSourceSecondsSinceLastEventType(kCGEventSourceStateCombinedSessionState, kCGEventFlagsChanged);
-        if (since > 5.0) {
-            memset(state, 0, sizeof(struct kb_state));
-        }
-
-        CGEventFlags newflags = flags;
-        if (state->pedal_ctrl_down)
-            newflags |= kCGEventFlagMaskControl;
-
-        if (state->pedal_alt_down)
-            newflags |= kCGEventFlagMaskAlternate;
-
-        if (state->pedal_cmd_down)
-            newflags |= kCGEventFlagMaskCommand;
-
-        if (flags != newflags) {
-            if (debug) printf("Modifying flags!!\n");
-            if (dry_run) CGEventSetFlags(event, newflags);
-        }
-
-    }
-    fflush(stdout);
-
-    return event;
-}
-
-struct option options[] = {
+const struct option options[] = {
     {
         .name = "debug",
         .has_arg = no_argument,
@@ -72,36 +26,73 @@ struct option options[] = {
         .flag = &dry_run,
         .val = 1,
     },
+    {
+        .name = "help",
+        .has_arg = no_argument,
+        .flag = &help,
+        .val = 1,
+    }
 };
+
+void describe(char *msg, CGEventFlags state) {
+    fprintf(stderr, "%s: %d, %d, %d\n", msg, !!(state & kCGEventFlagMaskControl), !!(state & kCGEventFlagMaskAlternate), !!(state & kCGEventFlagMaskCommand));
+}
+
+CGEventRef pedalCallback(CGEventTapProxy proxy __attribute__((unused)), CGEventType type, CGEventRef event, void *info) {
+    CGEventFlags *state = (CGEventFlags *)info;
+    CGEventFlags flags = CGEventGetFlags(event);
+    if (type == kCGEventFlagsChanged) {
+        if (debug) describe("before", *state);
+        *state = flags & (kCGEventFlagMaskControl | kCGEventFlagMaskAlternate | kCGEventFlagMaskCommand);
+        if (debug) describe("after", *state);
+
+    } else if (type == kCGEventKeyDown) {
+        const CFTimeInterval cooldown = 10.0;
+        CFTimeInterval since = CGEventSourceSecondsSinceLastEventType(kCGEventSourceStateCombinedSessionState,
+                                                                      kCGEventFlagsChanged);
+        if (since > cooldown) {
+            if (debug) fprintf(stderr, "Resetting flags!!\n");
+            *state = 0;
+        }
+
+        if (*state) {
+            if (debug) fprintf(stderr, "Modifying flags!!\n");
+            if (dry_run) CGEventSetFlags(event, flags | *state);
+        }
+
+    }
+    return event;
+}
 
 int main(int argc, char * const * argv) {
     while (getopt_long(argc, argv, "", options, NULL) != -1);
+    if (help) {
+        printf("Usage: centripedal [--debug] [--dry-run]\n");
+        return 0;
+    }
+
     if (debug) printf("Booting up…\n");
-    struct kb_state state = {0, 0, 0};
-    CGEventMask mask = CGEventMaskBit(kCGEventKeyDown) | CGEventMaskBit(kCGEventFlagsChanged);
-    CFMachPortRef tap = CGEventTapCreate(kCGHIDEventTap, kCGHeadInsertEventTap, kCGEventTapOptionDefault, mask, pedalCallback, &state);
+
+    CGEventFlags pressedModifierKeys = 0;
+    CFMachPortRef tap = CGEventTapCreate(kCGHIDEventTap, kCGHeadInsertEventTap,
+                                         kCGEventTapOptionDefault,
+                                         CGEventMaskBit(kCGEventKeyDown) | CGEventMaskBit(kCGEventFlagsChanged),
+                                         pedalCallback, &pressedModifierKeys);
     if (tap == NULL) {
-      fprintf(stderr, "couldn't create tap\n");
+        fprintf(stderr, "couldn't create tap, may need accessibility privileges?\n");
         exit(EXIT_FAILURE);
     } else if (debug) {
-        printf("tap created\n");
+        fprintf(stderr, "tap created\n");
     }
 
     CFRunLoopSourceRef source = CFMachPortCreateRunLoopSource(NULL, tap, 0);
     if (source == NULL) {
-        printf("Couldn't create runloop source");
-        exit(EXIT_FAILURE);
-    }
-    CFRunLoopRef runloop = CFRunLoopGetCurrent();
-    if (runloop == NULL) {
-        printf("Couldn't create runloop");
+        fprintf(stderr, "Couldn't create runloop source");
         exit(EXIT_FAILURE);
     }
 
-    CFRunLoopAddSource(runloop, source, kCFRunLoopCommonModes);
-
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), source, kCFRunLoopCommonModes);
     CFRunLoopRun();
     CFRelease(source);
-
     return 0;
 }
